b09310ece2c0b9a8599da8d7f5a0807790116fca
fyky <fuyu12dmb25ms@gmail.com>
2021-10-12 04:14:00 +0000

    [Finish]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,8 +17,15 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blogs_path(blog.id)
   end
-
+  
   def edit
+    @blog=Blog.find(params[:id])
+  end
+  
+  def update
+    blog=Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
   end
   
   private
